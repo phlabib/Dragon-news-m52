8ec023267958c59c1cc90f99f249be0e0ef096a5
phlabib <ph.labib@gmail.com>
2023-10-03 20:32:53 +0600

    52-6 Homepage layout and date time formatting using moment.js

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,16 +1,27 @@
 import Header from "../sheard/Header/Header";
+import LeftSideNav from "../sheard/LeftSideNav/LeftSideNav";
 import Navbar from "../sheard/Navbar";
-
+import RightSideNav from "../sheard/RightSideNav/RightSideNav";
 
 const Home = () => {
-    return (
-        <div>
-            <Header> </Header>
-            <Navbar></Navbar>
+  return (
+    <div>
+      <Header> </Header>
+      <Navbar></Navbar>
 
-            <h2 className="text-3xl font-poppins">This is Home</h2>
+      <div className="grid grid-cols-1 md:grid-cols-4 gap-6">
+        <div className="border">
+          <LeftSideNav></LeftSideNav>
         </div>
-    );
+        <div className="md:col-span-2 border">
+          <h2 className="text-4xl">News Comming Soon</h2>
+        </div>
+        <div className="border">
+          <RightSideNav></RightSideNav>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
