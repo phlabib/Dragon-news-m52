ed7fb8eef77a9c6683c6b579151f7131347db2dc
phlabib <ph.labib@gmail.com>
2023-10-03 17:42:45 +0600

    52-5 Custom Tailwind class and font setup

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,14 @@
+import Header from "../sheard/Header/Header";
+import Navbar from "../sheard/Navbar";
 
 
 const Home = () => {
     return (
         <div>
-            <h2 className="text-3xl">This is Home</h2>
+            <Header> </Header>
+            <Navbar></Navbar>
+
+            <h2 className="text-3xl font-poppins">This is Home</h2>
         </div>
     );
 };
